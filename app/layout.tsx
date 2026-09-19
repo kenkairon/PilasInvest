9f9955360582e69379ba34177f8b6e585a3e6fb1
kenkairon <ingeinfo2006@gmail.com>
2026-09-18 21:53:54 -0300

    prueba de cambios de device

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Archivo, IBM_Plex_Mono } from "next/font/google";
 import "./globals.css";
 
-
 const archivo = Archivo({
   subsets: ["latin"],
   variable: "--font-archivo",
