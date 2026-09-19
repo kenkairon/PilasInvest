2d612907d90a82357942564bf365ed57549770a7
kenkairon <ingeinfo2006@gmail.com>
2026-09-18 21:41:22 -0300

    cambio ap

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Archivo, IBM_Plex_Mono } from "next/font/google";
 import "./globals.css";
 
+
 const archivo = Archivo({
   subsets: ["latin"],
   variable: "--font-archivo",
